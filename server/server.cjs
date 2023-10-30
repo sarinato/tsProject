8b8e7a7219dc4462293be5c7d77834e27cecb79a
Abderrahmane El Filali <41301561+sarinato@users.noreply.github.com>
2023-10-30 17:25:24 +0100

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-app.use(cors());
+app.use(cors()) {data};
 
 app.use('/login', (req, res) => {
   res.send({
